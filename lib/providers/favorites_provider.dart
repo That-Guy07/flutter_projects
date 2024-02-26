1e50aa1140a428840888087fc403a014990b1c2a
That-Guy07 <shahanuralam861@hotmail.com>
2024-02-26 16:08:22 +0530

    184/1:52 / ReaverPod is integrated

---
+++ lib/providers/favorites_provider.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals/models/meal.dart';
+
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealsNotifier() : super([]);
+
+  void toggleFavoriteStatus(Meal meal) {
+    final mealisFavorite = state.contains(meal);
+
+    if (mealisFavorite) {
+      state = state.where((m) => m.id != meal.id).toList();
+    } else {
+      state = [...state, meal];
+    }
+    state = [];
+  }
+}
+
+final favoriteMealsProvider =
+    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>((ref) {
+  return FavoriteMealsNotifier();
+});
